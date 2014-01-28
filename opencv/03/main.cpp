6a6eb828ec6b8ce1602e65d74f0de01c16239814
Yang Yang <geraint0923@gmail.com>
2014-01-28 20:39:02 +0800

    Add the third part of opencv

---
+++ opencv/03/main.cpp
@@ -0,0 +1,40 @@
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+IplImage *pImage, *pDstImage;
+
+const char *pDstWindowTitle = "Edge Detection";
+
+void on_trackbar(int threshold) {
+	cvCanny(pImage, pDstImage, threshold, threshold * 3, 3);
+	cvShowImage(pDstWindowTitle, pDstImage);
+}
+
+int main() {
+	const char *pImageName = "/Users/yy/Pictures/add.png";
+	const char *pWindowTitle = "Origin Image";
+	const char *pWindowToolBar = "Threshold";
+
+	pImage = cvLoadImage(pImageName, CV_LOAD_IMAGE_GRAYSCALE);
+	pDstImage = cvCreateImage(cvGetSize(pImage), IPL_DEPTH_8U, 1);
+
+	cvNamedWindow(pWindowTitle, CV_WINDOW_AUTOSIZE);
+	cvNamedWindow(pDstWindowTitle, CV_WINDOW_AUTOSIZE);
+
+	int nThresholdEdge = 1;
+	cvCreateTrackbar(pWindowToolBar, pDstWindowTitle, &nThresholdEdge, 100, on_trackbar);
+
+	cvShowImage(pWindowTitle, pImage);
+	on_trackbar(1);
+
+	cvWaitKey();
+
+	cvDestroyWindow(pWindowTitle);
+	cvDestroyWindow(pDstWindowTitle);
+
+	cvReleaseImage(&pImage);
+	cvReleaseImage(&pDstImage);
+
+	return 0;
+}
