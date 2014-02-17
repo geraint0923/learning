72e35a2f6cf9971c79d207d80909e3e09fa82f26
Yang Yang <geraint0923@gmail.com>
2014-02-18 03:43:50 +0800

    Add cmake leanring code

---
+++ cmake/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "test.h"
+
+using namespace std;
+
+int main() {
+	cout<<"Result: "<<test(2, 3)<<endl;
+	return 0;
+}
---
+++ cmake/test/test.cpp
@@ -0,0 +1,10 @@
+
+#include "test.h"
+#include <iostream>
+
+using namespace std;
+
+int test(int a, int b) {
+	cout<<"Test: "<<a<<" + "<<b<<" = "<<(a+b)<<endl;
+	return a + b;
+}
